b30ad16c722bd558a83503b98adc64559e211e60
Your Name <you@example.com>
2021-11-25 18:59:53 -0300

    feito as duas páginas

--- userPage/script.js
+++ userPage/script.js
@@ -1,6 +1,6 @@
 const username = document.location.search.replace('?', '');
 
-fetch('https://fake-github.herokuapp.com/api/' + username).then(function (resultado) {
+fetch('https://fake-github.herokuapp.com/api/search/' + username).then(function (resultado) {
     resultado.json().then(function (data) {
         const avatar = document.createElement("img")
         avatar.src = data.avatar_url;
@@ -17,9 +17,13 @@ fetch('https://fake-github.herokuapp.com/api/' + username).then(function (result
     console.log('Erro: ', erro);
 });
 
-fetch('https://fake-github.herokuapp.com/api/' + username + '/repos').then(function (resultado) {
+fetch('https://fake-github.herokuapp.com/api/search/' + username + '/repos').then(function (resultado) {
     resultado.json().then(function (data) {
-        data.forEach(get)
+        tabelaRepositorios();
+        data.forEach(function (e) {
+            inserirRepositorioTabela(e);
+        })
+        console.log("data: ", data)
     });
 }).catch(function (erro) {
     console.log('Erro: ', erro);
@@ -38,4 +42,29 @@ function tabelaRepositorios() {
     const colunaNome = document.createElement('th');
     const colunaLink = document.createElement('th');
 
+    document.body.appendChild(tabela);
+    tabela.appendChild(linhaHeader);
+    linhaHeader.appendChild(colunaNome);
+    linhaHeader.appendChild(colunaLink);
+
+    colunaNome.innerText = 'Nome';
+    colunaLink.innerText = 'Link';
+}
+
+function inserirRepositorioTabela(element) {
+    const linha = document.createElement('tr');
+    const colunaNome = document.createElement('td');
+    const colunaLink = document.createElement('td');
+    const link = document.createElement('a');
+
+    let tabela = document.querySelector('table');
+
+    colunaNome.innerText = element.name;
+    link.innerText = element.svn_url;
+    link.href = element.svn_url;
+
+    tabela.appendChild(linha);
+    linha.appendChild(colunaNome);
+    linha.appendChild(colunaLink);
+    colunaLink.appendChild(link);
 }
